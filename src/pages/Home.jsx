385a7d84b1fdda124a9369061e84206d0ad15dce
jcochran206 <jonathan.o.cochran@gmail.com>
2025-05-03 21:27:55 -0700

    light practice

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -14,7 +14,10 @@ const Home = () => {
         camera={{near: 0.1, far: 1000}}
         >
           <Suspense fallback={<Loader/>}>
-
+            <directionalLight/>
+            <ambientLight/>
+            <pointLight/>
+            <spotLight/>
           </Suspense>
         </Canvas>
     </section>
